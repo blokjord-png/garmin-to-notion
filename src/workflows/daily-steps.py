4c0f19df4ea89dc0f379515faa0d2d7b0701ab1f
blokjord-png <blokjord@gmail.com>
2026-06-28 22:02:53 +0200

    Update daily-steps.py

--- src/workflows/daily-steps.py
+++ src/workflows/daily-steps.py
@@ -9,7 +9,7 @@ def get_all_daily_steps(garmin):
     """
     Get last x days of daily step count data from Garmin Connect.
     """
-    startdate = date.today() - timedelta(days=1)
+    startdate = date.today() - timedelta(days=500)
     daterange = [startdate + timedelta(days=x)
                  for x in range((date.today() - startdate).days)]  # excl. today
     daily_steps = []
